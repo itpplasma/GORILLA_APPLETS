c99c4f1bae752681aee90c8f99b97bc3c2df80c9
jonatanschatzlmayr <schatzlmayr@student.tugraz.at>
2025-06-25 14:14:10 +0200

    enable calling of perform_electric_potential_update(0) refine calc_average_charge_density_per_flux_layer add to print_data bug fix in associate_flux_labels_with_tetrahedra_and_vertices refine prepare_next_round_of_parallelised_particle_pushing

--- SRC/utils_data_pre_and_post_processing_mod.f90
+++ SRC/utils_data_pre_and_post_processing_mod.f90
@@ -445,7 +445,7 @@ subroutine perform_electric_potential_update(i)
     use field_mod, only: ipert
 
     integer, intent(in) :: i
-    real(dp) :: r=0.5_dp !under-relaxation factor
+    real(dp) :: r=0.0_dp !under-relaxation factor
 
     if (boole_time_Hamiltonian.eqv..false.) then
         print*, "Error, variable 'boole_time_Hamiltonian' must be set to '.true.' for electric potential update to work"
@@ -454,12 +454,12 @@ subroutine perform_electric_potential_update(i)
 
     call calc_phi_elec_from_rho(i)
 
-    phi_elec = r*phi_elec + (1-r)*ep%phi_elec_from_rho
+    phi_elec = phi_elec + ep%phi_elec_from_rho
 
     !giving i_option = 12 as input variable prevents make_tetra_physics from overwriting phi_elec
     call make_tetra_physics(coord_system,ipert,i_option = 12)
 
-    call print_data(i)
+    if (i.gt.0) call print_data(i)
 
 end subroutine perform_electric_potential_update
 
@@ -476,23 +476,28 @@ subroutine calc_phi_elec_from_rho(i)
     ep%rho_vert = 0
     
     ep%phi_elec_from_rho = 0
-    ep%average_abs_phi_elec_from_rho(i) = 0
-    ep%total_tracing_time(i) = 0
+    if (i.gt.0) then
+        ep%average_abs_phi_elec_from_rho(i) = 0
+        ep%total_tracing_time(i) = 0
+    endif
     
     if (in%update_dimension.eq.1) then
 
         call calc_average_charge_density_per_flux_layer
         call calc_rho_on_vertices
 
-        if (i.eq.1) ep%mean_abs_rho_at_first_update = sum(abs(ep%rho_vert))/size(ep%rho_vert)
+        ! if (i.eq.1) ep%mean_abs_rho_at_first_update = sum(abs(ep%rho_vert))/size(ep%rho_vert)
 
-        if (ep%mean_abs_rho_at_first_update.gt.eps**2) then
-            factor = in%energy_eV*ev2erg/echarge/ep%mean_abs_rho_at_first_update
-        else
-            factor = 1.0_dp
-        endif
+        ! if (ep%mean_abs_rho_at_first_update.gt.eps**2) then
+        !     factor = in%energy_eV*ev2erg/echarge/ep%mean_abs_rho_at_first_update
+        ! else
+        !     factor = 1.0_dp
+        ! endif
 
-        ep%phi_elec_from_rho = factor * ep%rho_vert
+        factor = 21.8905_dp
+        factor = in%energy_eV*ev2erg/(echarge**2*in%density)!T/(n_0*e^2)= 4*pi*r_D^2
+
+        ep%phi_elec_from_rho =  ep%rho_vert*factor
         
     endif
 
@@ -506,26 +511,48 @@ subroutine calc_average_charge_density_per_flux_layer
     use constants, only: echarge
 
     integer :: ns, j, species, ind_prism, n_species
-    real(dp) :: prism_volumes, s
+    real(dp), dimension(:), allocatable :: s_shell_volumes, electron_densities
+    real(dp) :: s, electron_density_factor
 
     n_species = in%n_species
-    if (in%boole_static_ne) species = in%n_species-1
+
+    allocate(s_shell_volumes(grid_size(1)),electron_densities(grid_size(1)))
+    s_shell_volumes = 0.0_dp
+
+    !Compute s_shell_volumes and if in%boole_static_ne compute electron_densities
+    do ns = 1,grid_size(1)
+        s = (verts_sthetaphi(1, grid_size(3)*(ns-1)+1) + verts_sthetaphi(1,grid_size(3)*ns+1))/2.0_dp
+        if (in%boole_static_ne) electron_densities(ns) = in%density*(1.0_dp-0.9_dp*s)
+        do j = 1, grid_size(2)*grid_size(3)*2
+            ind_prism = g%prisms_per_flux_tube(ns,j)
+            s_shell_volumes(ns) = s_shell_volumes(ns) + output%prism_volumes(ind_prism)
+        enddo
+    enddo
+
+    if (in%boole_static_ne) then
+        n_species = in%n_species-1
+        if (in%boole_linear_density_simulation) then
+            electron_density_factor = 1.0d0
+        else
+            electron_density_factor = in%density/(sum(electron_densities*s_shell_volumes)/sum(s_shell_volumes))
+        endif
+    endif
 
     do ns = 1,grid_size(1)
-        prism_volumes = 0
         s = (verts_sthetaphi(1, grid_size(3)*(ns-1)+1) + verts_sthetaphi(1,grid_size(3)*ns+1))/2.0_dp
         do j = 1, grid_size(2)*grid_size(3)*2
             ind_prism = g%prisms_per_flux_tube(ns,j)
-            prism_volumes = prism_volumes + output%prism_volumes(ind_prism)
             do species = 1,n_species
                 ep%rho_flux_layer(ns) = ep%rho_flux_layer(ns) + real(output%prism_moments(1,ind_prism,species))* &
                                         output%prism_volumes(ind_prism)*start%particle_charge(species)
             enddo
-            if (in%boole_static_ne) ep%rho_flux_layer(ns) = ep%rho_flux_layer(ns) + &
-                                                            output%prism_volumes(ind_prism)*in%density*(-echarge)*(1.0_dp-0.9_dp*s)
+            if (in%boole_static_ne) ep%rho_flux_layer(ns) = ep%rho_flux_layer(ns) + electron_density_factor*electron_densities(ns)*&
+                                                            output%prism_volumes(ind_prism)*(-echarge)
         enddo
-        ep%rho_flux_layer(ns) = ep%rho_flux_layer(ns)/prism_volumes
+        ep%rho_flux_layer(ns) = ep%rho_flux_layer(ns)/s_shell_volumes(ns)
     enddo
+
+    print*, 'electron_density_factor = ', electron_density_factor
 
 end subroutine calc_average_charge_density_per_flux_layer
 
@@ -558,7 +585,8 @@ subroutine calc_rho_on_vertices
     do ns = 2,grid_size(1),2
         value_to_be_set = 0.5_dp*(ep%rho_flux_layer(ns-1)+ep%rho_flux_layer(ns))
         !remove this line in the future >
-        !value_to_be_set = -sin(dble(ns-1)/dble(grid_size(1)-1)*pi)
+        !value_to_be_set = sin(dble(ns-1)/dble(grid_size(1)-1)*pi)
+        !value_to_be_set = dble(ns)/dble(grid_size(1))
         call fill_vector_parts_with_value(ep%rho_vert, g%vertices_per_flux_surface(ns,:), value_to_be_set)
         rho_per_flux_surface(ns) = value_to_be_set
     enddo
@@ -610,20 +638,47 @@ end subroutine calc_rho_on_vertices
 
 subroutine print_data(i)
 
-    use gorilla_applets_types_mod, only: c, output, grid_t, in, ep, exit_data
+    use gorilla_applets_types_mod, only: c, output, grid_t, in, ep, exit_data, counter, s
     use tetra_physics_mod, only: phi_elec
+    use tetra_grid_settings_mod, only: grid_size
+    use tetra_physics_mod, only: tetra_physics
 
     integer, intent(in) :: i
-    integer :: ep_unit, ed_unit, pe_unit, id_unit, j, species
-    character(len=100) :: filename_ep, filename_ed, i_str, filename_phi_elec, filename_ion_densities
+    integer :: ep_unit, ed_unit, pe_unit, id_unit, l_unit, ef_unit, s_unit, j, species, k
+    character(len=100) :: filename_ep, filename_ed, i_str, filename_phi_elec, filename_ion_densities, filename_lost, filename_ef, &
+                          filename_s
+
+    if (i.eq.1) then
+        filename_ef = 'electric_field.dat'
+        open(newunit = ef_unit, file = filename_ef)
+        do j = 1,grid_size(1)
+            write(ef_unit,*) tetra_physics((j-1)*6*grid_size(2)+1)%Er_mod
+        enddo
+        close(ef_unit)
+    endif
+    
+    filename_lost = 'number_of_lost_particles.dat'
+    if (i.eq.1) call unlink(filename_lost)
+    open(newunit = l_unit, file = filename_lost,position = 'append')
+    write(l_unit,*) i, counter%lost_particles
+    close(l_unit)
 
     write(i_str, '(I0)') i  ! Convert integer to string without leading spaces
 
     filename_ep = 'rho_per_vertex_during_electric_potential_update_' // trim(i_str) // '.dat'
     call unlink(filename_ep)
-    open(newunit = ep_unit, file = filename_ep)
-    write(ep_unit,'(ES20.10E4)') ep%rho_vert
-    close(ep_unit)
+    !open(newunit = ep_unit, file = filename_ep)
+    !write(ep_unit,'(ES20.10E4)') ep%rho_vert
+    !close(ep_unit)
+
+    ! filename_s = 's_statistics_after_electric_potential_update' // trim(i_str) // '.dat'
+    ! call unlink(filename_s)
+    ! k = size(s%time)
+    ! open(newunit = s_unit, file = filename_s)
+    ! do j = 1,k
+    !     write(s_unit,*) s%time(j), s%delta_s(j), s%delta_s_squared(j), s%check(j)
+    ! enddo
+    ! close(s_unit)
 
     filename_phi_elec = 'phi_elec_after_electric_potential_update_' // trim(i_str) // '.dat'
     call unlink(filename_phi_elec)
@@ -637,16 +692,16 @@ subroutine print_data(i)
     write(id_unit,'(ES20.10E4)') real(output%prism_moments(1,:,1))
     close(id_unit)
 
-    filename_ed = 'exit_data_' // trim(i_str) // '.dat'
-    call unlink(filename_ed)
-    open(newunit = ed_unit, file = filename_ed)
-    do j=1,in%num_particles
-        write(ed_unit,*) exit_data%t_confined(j,1), dble(exit_data%integration_step(j,1))
-    enddo
-    do j=1,in%num_particles
-        write(ed_unit,*) exit_data%t_confined(j,2), dble(exit_data%integration_step(j,2))
-    enddo
-    close(ed_unit)
+    ! filename_ed = 'exit_data_' // trim(i_str) // '.dat'
+    ! call unlink(filename_ed)
+    ! open(newunit = ed_unit, file = filename_ed)
+    ! do j=1,in%num_particles
+    !     write(ed_unit,*) exit_data%t_confined(j,1), dble(exit_data%integration_step(j,1)), exit_data%x
+    ! enddo
+    ! do j=1,in%num_particles
+    !     write(ed_unit,*) exit_data%t_confined(j,2), dble(exit_data%integration_step(j,2)), exit_data%x
+    ! enddo
+    ! close(ed_unit)
 
     do j = 1,in%num_particles
         do species = 1,2
@@ -682,8 +737,8 @@ subroutine associate_flux_labels_with_tetrahedra_and_vertices
     !Fill vertices_per_flux_surface
     do ns = 1,grid_size(1)+1
         i = 1
-        do nphi = 1,grid_size(3)
-            do ntheta = 1,grid_size(2)
+        do nphi = 1,grid_size(2)
+            do ntheta = 1,grid_size(3)
                 ind_vert = (nphi-1)*(grid_size(1)+1)*grid_size(3) + (ns-1)*grid_size(3) + ntheta
                 g%vertices_per_flux_surface(ns,i) = ind_vert
                 i = i + 1
@@ -694,8 +749,8 @@ subroutine associate_flux_labels_with_tetrahedra_and_vertices
     !Fill prisms_per_flux_tube
     do ns = 1,grid_size(1)
         i = 1
-        do nphi = 1,grid_size(3)
-            do ntheta = 1,grid_size(2)
+        do nphi = 1,grid_size(2)
+            do ntheta = 1,grid_size(3)
                 do j = 1,2 !count through prisms per hexahedron
                     ind_prism = (nphi-1)*grid_size(1)*grid_size(3)*2 + (ns-1)*grid_size(3)*2 + (ntheta-1)*2 + j
                     g%prisms_per_flux_tube(ns,i) = ind_prism
@@ -709,7 +764,7 @@ end subroutine associate_flux_labels_with_tetrahedra_and_vertices
 
 subroutine prepare_next_round_of_parallelised_particle_pushing(species)
 
-    use gorilla_applets_types_mod, only: start, output, moment_specs
+    use gorilla_applets_types_mod, only: start, output, moment_specs, counter
     use constants, only: echarge,ame,clight
     use tetra_physics_mod, only: cm_over_e, particle_charge, particle_mass
     use gorilla_applets_settings_mod, only: i_option
@@ -730,6 +785,12 @@ subroutine prepare_next_round_of_parallelised_particle_pushing(species)
     output%tetr_moments(:,:,species) = 0.0_dp
     output%prism_moments(:,:,species) = 0.0_dp
     if (moment_specs%boole_squared_moments) output%prism_moments_squared(:,:,species) = 0.0_dp
+
+    counter%lost_particles = 0
+    counter%lost_inside = 0
+    counter%tetr_pushings = 0
+    counter%phi_0_mappings = 0
+    counter%integration_steps = 0
 
 end subroutine prepare_next_round_of_parallelised_particle_pushing
 
