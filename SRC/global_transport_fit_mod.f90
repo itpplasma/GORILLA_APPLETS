ddc9e3ca205b81112b2bf610aa51a22814323a72
Christopher Albert <albert@tugraz.at>
2026-03-29 10:42:31 +0200

    add lm global transport fitting workflow

---
+++ SRC/global_transport_fit_mod.f90
@@ -0,0 +1,46 @@
+module global_transport_fit_mod
+
+    use, intrinsic :: iso_fortran_env, only: dp => real64
+
+    implicit none
+
+contains
+
+subroutine calc_global_transport_fit()
+
+    use global_transport_fit_core_mod, only: fit_global_transport
+    use global_transport_fit_io_mod, only: load_global_transport_experiments, write_global_transport_fit_outputs
+    use global_transport_fit_settings_mod, only: control, filename_boundary_s, filename_density_1, filename_density_2, &
+        filename_density_variance_1, filename_density_variance_2, filename_fit_profiles, filename_fit_summary, &
+        filename_flux_1, filename_flux_2, filename_flux_variance_1, filename_flux_variance_2, filename_shell_volumes, &
+        filename_source_1, filename_source_2, load_global_transport_fit_inp
+    use global_transport_fit_types_mod, only: global_transport_experiment_t, global_transport_fit_result_t
+
+    type(global_transport_experiment_t), allocatable :: experiments(:)
+    type(global_transport_fit_result_t) :: result
+    character(len=256), dimension(2) :: density_files
+    character(len=256), dimension(2) :: density_var_files
+    character(len=256), dimension(2) :: flux_files
+    character(len=256), dimension(2) :: flux_var_files
+    character(len=256), dimension(2) :: source_files
+
+    call load_global_transport_fit_inp()
+    source_files = [filename_source_1, filename_source_2]
+    density_files = [filename_density_1, filename_density_2]
+    density_var_files = [filename_density_variance_1, filename_density_variance_2]
+    flux_files = [filename_flux_1, filename_flux_2]
+    flux_var_files = [filename_flux_variance_1, filename_flux_variance_2]
+
+    call load_global_transport_experiments(filename_boundary_s, filename_shell_volumes, source_files, density_files, &
+        density_var_files, flux_files, flux_var_files, experiments)
+    call fit_global_transport(experiments, control, result)
+    call write_global_transport_fit_outputs(experiments(1)%boundary_s, result, filename_fit_summary, filename_fit_profiles)
+
+    print *, 'Global transport fit complete.'
+    print *, 'objective = ', result%objective
+    print *, 'converged = ', result%converged
+    print *, 'iterations = ', result%n_iterations
+
+end subroutine calc_global_transport_fit
+
+end module global_transport_fit_mod
---
+++ SRC/global_transport_fit_types_mod.f90
@@ -0,0 +1,52 @@
+module global_transport_fit_types_mod
+
+    use, intrinsic :: iso_fortran_env, only: dp => real64
+
+    implicit none
+
+    private
+
+    public :: global_transport_experiment_t
+    public :: global_transport_fit_control_t
+    public :: global_transport_fit_result_t
+
+    type global_transport_experiment_t
+        real(dp), allocatable :: boundary_s(:)
+        real(dp), allocatable :: shell_volumes(:)
+        real(dp), allocatable :: source(:)
+        real(dp), allocatable :: density(:)
+        real(dp), allocatable :: density_variance(:)
+        real(dp), allocatable :: flux(:)
+        real(dp), allocatable :: flux_variance(:)
+    end type global_transport_experiment_t
+
+    type global_transport_fit_control_t
+        integer :: n_knots_a = 5
+        integer :: n_knots_b = 5
+        integer :: max_lm_iterations = 30
+        real(dp) :: regularization_a = 1.0d-8
+        real(dp) :: regularization_b = 1.0d-8
+        real(dp) :: lm_damping = 1.0d-4
+        real(dp) :: lm_damping_increase = 10.0d0
+        real(dp) :: lm_damping_decrease = 0.3d0
+        real(dp) :: step_tolerance = 1.0d-10
+        real(dp) :: gradient_tolerance = 1.0d-10
+        real(dp) :: outer_boundary_density = 0.0d0
+    end type global_transport_fit_control_t
+
+    type global_transport_fit_result_t
+        logical :: converged = .false.
+        integer :: n_iterations = 0
+        real(dp) :: objective = 0.0d0
+        real(dp), allocatable :: parameters(:)
+        real(dp), allocatable :: gradient(:)
+        real(dp), allocatable :: covariance(:,:)
+        real(dp), allocatable :: a_profile(:)
+        real(dp), allocatable :: b_profile(:)
+        real(dp), allocatable :: a_std(:)
+        real(dp), allocatable :: b_std(:)
+        real(dp), allocatable :: knot_s_a(:)
+        real(dp), allocatable :: knot_s_b(:)
+    end type global_transport_fit_result_t
+
+end module global_transport_fit_types_mod
